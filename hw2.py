90e8c5eb457a9b37833ee0dda88c5a11e1cd31bc
tzvikif <tzvikif@gmail.com>
2020-05-13 22:58:28 +0300

    more changes

--- hw2.py
+++ hw2.py
@@ -8,6 +8,7 @@ from torch import nn, optim
 import torch.nn.functional as F
 
 SHOW_SAMPLES=False
+LOAD_WEIGHTS=True
 EPOCHS = 10
 
 transform = transforms.Compose([transforms.ToTensor(),
@@ -63,8 +64,50 @@ def freezeFirstLayer():
             param.requires_grad = True
 def initThirdLayer():
     for name,param in model.named_parameters():
-        if name in ['fc3.weight','fc3.bias']:
-            torch.nn.init.xavier_uniform(param)
+        if name in ['fc3.weight']:
+            nn.init.xavier_uniform_(param)
+def saveWeights(model,path='weights_only.pth'):
+    torch.save(model.state_dict(),path)
+def loadWeights(path='weights_only.pth'):
+    model_new = Net(D=input_size, H1=128, H2=64, Classes=10)
+    model_new.load_state_dict(torch.load(path))
+    return model_new
+def trainModel(model,zeroToSix=True):
+    model.train()
+    criterion = nn.NLLLoss()
+    #optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
+    optimizer = optim.Adam(model.parameters())
+    time0 = time()
+    for e in range(EPOCHS):
+        running_loss = 0
+        for images, labels in trainloader:
+            images06,labels06,images79,labels79 = exractDigits(images,labels)
+            # Flatten MNIST images into a 784 long vector
+            if(zeroToSix):
+                images = images06
+                labels = labels06
+            else:
+                images = images79
+                labels = labels79
+            images = images.view(images.shape[0], -1)
+        
+            # Training pass
+            optimizer.zero_grad()
+            
+            output = model(images)
+            #labels = torch.tensor([2,1,2,3,4,5,6,2,3,4,4,5,6])
+            loss = criterion(output, labels)
+            
+            #This is where the model learns by backpropagating
+            loss.backward()
+            
+            #And optimizes its weights here
+            optimizer.step()
+            
+            running_loss += loss.item()
+        else:
+            print("Epoch {} - Training loss: {}".format(e, running_loss/len(trainloader)))
+            print("\nTraining Time (in minutes) =",(time()-time0)/60)
 class Net(nn.Module):
     def __init__(self, D, H1, H2, Classes):
         super(Net, self).__init__()
@@ -76,7 +119,6 @@ class Net(nn.Module):
         self.dropout1 = nn.Dropout2d(0.25)
         self.dropout2 = nn.Dropout2d(0.25)
         
-
     def forward(self, x):
         x = self.fc1(x)
         x = self.bn1(x)
@@ -88,63 +130,45 @@ class Net(nn.Module):
         x = self.dropout2(x)
         x = self.fc3(x)
         return F.log_softmax(x,dim=1)
-
+def test(model,zeroToSix=True):
+    model.eval()
+    correct_count, all_count = 0, 0
+    for images,labels in valloader:
+        images06,labels06,images79,labels79 = exractDigits(images,labels) 
+        if(zeroToSix):
+            images = images06
+            labels = labels06
+        else:
+            images06 = images79
+            labels = labels79
+        for i in range(len(labels)):
+            img = images[i].view(1, 784)
+            with torch.no_grad():
+                logps = model(img)
+            ps = torch.exp(logps)
+            probab = list(ps.numpy()[0])
+            pred_label = probab.index(max(probab))
+            true_label = labels.numpy()[i]
+            if(true_label == pred_label):
+                correct_count += 1
+            all_count += 1
+    print("Number Of Images Tested =", all_count)
+    print("\nModel Accuracy =", (correct_count/all_count))
 
 torch.manual_seed(42)
-model = Net(D=input_size, H1=128, H2=64, Classes=7)
-
-model.train()
-
-criterion = nn.NLLLoss()
-
-#optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
-optimizer = optim.Adam(model.parameters())
-
-time0 = time()
-
-for e in range(EPOCHS):
-    running_loss = 0
-    for images, labels in trainloader:
-        images06,labels06,images79,labels79 = exractDigits(images,labels)
-        # Flatten MNIST images into a 784 long vector
-        images06 = images06.view(images06.shape[0], -1)
-    
-        # Training pass
-        optimizer.zero_grad()
-        
-        output = model(images06)
-        loss = criterion(output, labels06)
-        
-        #This is where the model learns by backpropagating
-        loss.backward()
-        
-        #And optimizes its weights here
-        optimizer.step()
-        
-        running_loss += loss.item()
-    else:
-        print("Epoch {} - Training loss: {}".format(e, running_loss/len(trainloader)))
-print("\nTraining Time (in minutes) =",(time()-time0)/60)
-
-
-
-model.eval()
-correct_count, all_count = 0, 0
-for images,labels in valloader:
-  images06,labels06,images79,labels79 = exractDigits(images,labels)  
-  for i in range(len(labels06)):
-    img = images06[i].view(1, 784)
-    with torch.no_grad():
-        logps = model(img)
-
-    
-    ps = torch.exp(logps)
-    probab = list(ps.numpy()[0])
-    pred_label = probab.index(max(probab))
-    true_label = labels06.numpy()[i]
-    if(true_label == pred_label):
-      correct_count += 1
-    all_count += 1
-
-print("Number Of Images Tested =", all_count)
-print("\nModel Accuracy =", (correct_count/all_count))
+model = Net(D=input_size, H1=128, H2=64, Classes=10)
+if(not LOAD_WEIGHTS):
+    trainModel(model,zeroToSix=True)
+    saveWeights()
+else:
+    model = loadWeights()
+test(model,zeroToSix=True)
+freezeFirstLayer()
+initThirdLayer()
+trainModel(model,zeroToSix=False)
+saveWeights(model=model,'seven_to_nine.pth')
+test(model,zeroToSix=False)
+print('=====fresh model======')
+fresh_model = loadWeights()
+trainModel(fresh_model,zeroToSix=False)
+test(fresh_model)
