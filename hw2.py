96e8b6c3cc1abe77a1dad1f7dcbcf4e50c6cac8f
tzvikif <tzvikif@gmail.com>
2020-05-18 22:15:21 +0300

    with save/load weights

--- hw2.py
+++ hw2.py
@@ -7,7 +7,9 @@ from torchvision import datasets, transforms
 from torch import nn, optim
 import torch.nn.functional as F
 
-SHOW_SAMPLES=True
+SHOW_SAMPLES=False
+LOAD_WEIGHTS = False
+SAVE_WEIGHTS = False
 EPOCHS = 5
 
 transform = transforms.Compose([transforms.ToTensor(),
@@ -31,15 +33,14 @@ print(images.shape)
 print(labels.shape)
 
 if (SHOW_SAMPLES):
-  plt.imshow(images[0].numpy().squeeze(), cmap='gray_r')
-
-  figure = plt.figure()
-  num_of_images = 60
-  for index in range(1, num_of_images + 1):
-      plt.subplot(6, 10, index)
-      plt.axis('off')
-      plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
-
+    plt.imshow(images[0].numpy().squeeze(), cmap='gray_r')
+    figure = plt.figure()
+    num_of_images = 60
+    for index in range(1, num_of_images + 1):
+        plt.subplot(6, 10, index)
+        plt.axis('off')
+        plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
+    plt.show()
 
 # image size is 28x28
 input_size = 784
@@ -77,13 +78,16 @@ def trainModel(model,trainloader,valSet):
     optimizer = optim.Adam(model.parameters())
     time0 = time()
     train_losses = list()
-    val_losses = list()
+    test_losses = list()
     for e in range(EPOCHS):
-        singleEpoch(model,optimizer,criterion,trainloader_0t6,valloader_0t6,e)
-        
+        train_loss,test_loss = singleEpoch(model,optimizer,criterion,trainloader_0t6,valloader_0t6,e)
+        train_losses.append(train_loss)
+        test_losses.append(test_loss)
+    return train_losses,test_losses
 def singleEpoch(model,optimizer,criterion,trainSet,valSet,e):
     running_loss = 0
     for images, labels in trainSet:
+        model.train()
             # Flatten MNIST images into a 784 long vector
         images = images.view(images.shape[0], -1)
     
@@ -102,6 +106,7 @@ def singleEpoch(model,optimizer,criterion,trainSet,valSet,e):
         running_loss += loss.item()
     val_batch_cnt = 0
     val_running_loss = 0
+    model.eval()
     for val_batch_idx ,(val_images, val_labels) in enumerate(valSet):
             val_images = val_images.view(val_images.shape[0], -1)
             val_output = model(val_images)
@@ -112,25 +117,52 @@ def singleEpoch(model,optimizer,criterion,trainSet,valSet,e):
                 break
     print("Epoch {} - Training loss: {}".format(e, running_loss/len(trainSet)))
     print("Test loss: {}".format(val_running_loss/val_batch_cnt))
+    return running_loss/len(trainSet),val_running_loss/val_batch_cnt
+def Multiplot(l,xlabel,ylabel,title=''):
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    for p in l:
+        x = p['x']
+        y = p['y']
+        funcName = p['funcName']
+        plt.plot(x,y,label = funcName)
+        plt.legend()
+        plt.title(title)
+        plt.plot()
+    plt.show()
+
+def testModel(model,valSet):
+    model.eval()
+    correct_count, all_count = 0, 0
+    for images,labels in valSet:
+        for i in range(len(labels)):
+            img = images[i].view(1, 784)
+            with torch.no_grad():
+                logps = model(img)
+            ps = torch.exp(logps)
+            probab = list(ps.numpy()[0])
+            pred_label = probab.index(max(probab))
+            true_label = labels.numpy()[i]
+            if(true_label == pred_label):
+                correct_count += 1
+            all_count += 1
+    print("Number Of Images Tested =", all_count)
+    print("\nModel Accuracy =", (correct_count/all_count))
+def saveWeights(model,path='weights_only.pth'):
+    torch.save(model.state_dict(),path)
+def loadWeights(new_model,path='weights_only.pth'):
+    new_model.load_state_dict(torch.load(path))
+    return new_model
 torch.manual_seed(42)
 model = Net(D=input_size, H1=128, H2=64, Classes=10)
-trainModel(model,trainloader_0t6,valloader_0t6)
-model.eval()
-correct_count, all_count = 0, 0
-for images,labels in valloader_0t6:
-  for i in range(len(labels)):
-    img = images[i].view(1, 784)
-    with torch.no_grad():
-        logps = model(img)
-
-    
-    ps = torch.exp(logps)
-    probab = list(ps.numpy()[0])
-    pred_label = probab.index(max(probab))
-    true_label = labels.numpy()[i]
-    if(true_label == pred_label):
-      correct_count += 1
-    all_count += 1
-
-print("Number Of Images Tested =", all_count)
-print("\nModel Accuracy =", (correct_count/all_count))
+if(LOAD_WEIGHTS):
+    model = loadWeights(model)
+else:
+    train_losses,test_losses = trainModel(model,trainloader_0t6,valloader_0t6)
+    x = np.arange(1,len(train_losses)+1)
+    dtrainLosses = {'x':x,'y':train_losses,'funcName':'Train losses'}
+    dtestLosses = {'x':x,'y':test_losses,'funcName':'Test losses'}
+    Multiplot([dtrainLosses,dtestLosses],'#Epochs','Accuracy',title='Loss')
+if(SAVE_WEIGHTS):
+    saveWeights(model)
+testModel(model,valloader_0t6)
